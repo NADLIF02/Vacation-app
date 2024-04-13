6b332b3bf300123ff424a082afea5faa3051c1b4
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-13 21:02:28 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,35 +1,37 @@
-// server/server.js
-
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+// Charger les variables d'environnement
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Middleware pour gérer les requêtes CORS et JSON
 app.use(cors());
 app.use(bodyParser.json());
 
-// Connect to MongoDB
+// Connexion à MongoDB avec gestion d'erreur
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.error("MongoDB connection error: ", err));
 
-// API routes
+// Importation et utilisation des routes d'authentification
 const authRoutes = require('./routes/authRoutes');
 app.use('/api', authRoutes);
 
-// Serve React build files in production
+// Serve static files from the React application
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
+// Un gestionnaire "catchall" pour toutes les requêtes qui ne correspondent pas aux autres routes
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
 });
 
+// Démarrage du serveur
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
