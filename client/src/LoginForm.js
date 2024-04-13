4b376365552ebe9102a9d54cbe0674d752232927
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-13 20:56:05 +0200

    Update LoginForm.js

--- client/src/LoginForm.js
+++ client/src/LoginForm.js
@@ -1,4 +1,3 @@
-// src/components/LoginForm.js
 import React, { useState } from 'react';
 import axios from 'axios';
 
@@ -9,28 +8,26 @@ function LoginForm({ onLogin }) {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const { data } = await axios.post('http://172.20.10.2:5000/api/login', { username, password });
-      console.log('Login successful', data);
-      onLogin(data.token, username); // Trigger the onLogin function passed as a prop
+      const { data } = await axios.post('http://localhost:5000/api/login', { username, password });
+      onLogin({ username, token: data.token }); // Lever l'état d'authentification avec le token et le nom d'utilisateur
     } catch (error) {
-      console.error('Login failed', error);
+      console.error('Login failed:', error);
+      // Gérer les erreurs de connexion ici
     }
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Username:
-          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-        </label>
-        <label>
-          Password:
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </label>
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <button type="submit">Login</button>
+    </form>
   );
 }
 
