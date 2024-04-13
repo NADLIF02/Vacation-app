e727deb4fd31b654ccb9c12f0a909c6070400961
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-13 19:24:55 +0200

    Create User.js

---
+++ server/models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 8);
+  next();
+});
+
+UserSchema.methods.comparePassword = function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
