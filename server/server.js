3fdff1ea31ef45dc4512f409b7933943304601c5
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-13 17:12:45 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
-require('dotenv').config();
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,17 +10,14 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 });
 
-const vacationsRouter = require('./routes/vacations');
-app.use('/vacations', vacationsRouter);
-
 app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+  console.log(`Server running on port: ${port}`);
 });
