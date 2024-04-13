82e25f0a4494f0434f4a1b10671149d61481d7ee
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-13 20:00:50 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');  // Assurez-vous d'importer le module 'path'
 
-require('dotenv').config(); // Loads environment variables from .env file
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,20 +12,21 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.error("MongoDB connection error: ", err));
 
-// Define a route for the root URL
-app.get('/', (req, res) => {
-    res.send('Hello, welcome to the Vacation App!');
-});
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`);
-});
+// Routes API
 const authRoutes = require('./routes/authRoutes');
 app.use('/api', authRoutes);
 
+// Serve React build files in production
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
